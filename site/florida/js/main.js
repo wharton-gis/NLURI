dedae58b5e51b1a6369c373c087689a5469f6f8e
chuembucket <cchuemmler@gmail.com>
2023-02-17 17:44:48 -0500

    florida and natl maps start

---
+++ site/florida/js/main.js
@@ -0,0 +1,27 @@
+import { initializeMap, polygonLayerfun } from "./map.js";
+import { changeFilter } from "./mapcolors.js";
+
+let Map = initializeMap();
+polygonLayerfun(Map);
+
+function downloadInv() {
+    const fpath = '../../../data/national_results/FL_bg_20230117.geojson';
+    fetch(fpath)
+    .then(resp => resp.json())
+    .then(data => {
+        Map.polygonLayer.addData(data);
+    });
+}
+
+downloadInv();
+
+
+
+let overlayDropdown = document.querySelector('#overlay');
+
+overlayDropdown.addEventListener('click', () => {
+        let value = event.target.value;
+        changeFilter(value);
+});
+
+window.Map = Map;
---
+++ site/florida/js/mapcolors.js
@@ -0,0 +1,47 @@
+import { initstyle } from "./map.js";
+
+
+function changeFilter(value) {
+    if(value === 'off' ) {
+        Map.polygonLayer.eachLayer(layer => { layer.setStyle(initstyle(layer)) });
+    }
+    if(value === 'r_index' ) {
+        Map.polygonLayer.eachLayer(layer => { layer.setStyle(r_index_style(layer)) });
+    }
+    if(value === 'source' ) {
+        Map.polygonLayer.eachLayer(layer => { layer.setStyle(source_style(layer)) });
+    }
+}
+
+function r_index_style(feature) {
+    return {
+        fillColor: 'red'/*  feature => {
+        if(feature.source > 1) return 'green';
+        return 'blue';
+        } */,
+        
+        weight: 2,
+        opacity: 1,
+        color: 'NA',  //Outline color
+        fillOpacity: 1,
+    };
+}
+
+function source_cols(p){
+    if(p == "Observation") return 'green';
+    if(p == "Observation") return 'green';
+
+    return 'blue';
+}
+
+function source_style(feature) {
+    return {
+        fillColor: source_cols(feature.properties.source),        
+        weight: 2,
+        opacity: 1,
+        color: 'NA',  //Outline color
+        fillOpacity: 1,
+    };
+}
+
+export{ changeFilter };
